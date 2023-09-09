b07abfbd693e6645193c23988793c4c1456c3e3c
sinhayash9546 <sinhayash9546@gmail.com>
2023-09-09 12:27:22 +0530

    added search bar functionality

--- src/components/CardLayouts.jsx
+++ src/components/CardLayouts.jsx
@@ -11,10 +11,25 @@ import {
   Divider,
   ButtonGroup,
   Button,
+  Input,
 } from "@chakra-ui/react";
 import jsonData from "../utils/Hand_Disability_Products.json";
 function CardLayouts() {
   const [disability, setDisability] = useState();
+  
+  const [searchQuery, setSearchQuery] = useState("");
+  const [filteredProducts, setFilteredProducts] = useState(jsonData);
+
+
+  useEffect(() => {
+    // Filter products based on the search query
+    const filtered = jsonData.filter((product) =>
+      product["Product Name"].toLowerCase().includes(searchQuery.toLowerCase())
+    );
+    setFilteredProducts(filtered);
+  }, [searchQuery]);
+
+
   const cardStyle = {
     width: "300px", // Adjust the width as needed
     marginBottom: "20px",
@@ -39,36 +54,46 @@ function CardLayouts() {
 
   const handleChange = () => {};
   return (
-    <div style={{ display: "flex", flexWrap: "wrap", justifyContent: "space-between" }}>
-    {jsonData.map((product, index) => (
-      <Card key={index} style={cardStyle}>
-        <Image
-          src={product["Image URL"]}
-          alt={product["Product Name"]}
-          style={imageStyle}
-        />
-        <Stack mt="6" spacing="3">
-          <CardHeader>
-            <Heading size="md" style={titleStyle}>
-              {product["Product Name"]}
-            </Heading>
-          </CardHeader>
-          <CardBody>
-            <Text>{product["Product Price"]}</Text>
-          </CardBody>
-          <CardFooter>
-            <ButtonGroup spacing="2">
-              <Button variant="solid" colorScheme="blue">
-                Buy now
-              </Button>
-              {/* You can add more buttons or actions here */}
-            </ButtonGroup>
-          </CardFooter>
-        </Stack>
-      </Card>
-    ))}
-  </div>
-  );
+    <>
+    <Input
+      placeholder="Search products"
+      value={searchQuery}
+      onChange={(e) => setSearchQuery(e.target.value)}
+    />
+    <div
+      style={{ display: "flex", flexWrap: "wrap", justifyContent: "space-between" }}
+    >
+      {filteredProducts.map((product, index) => (
+        <Card key={index} style={cardStyle}>
+          <Image
+            src={product["Image URL"]}
+            alt={product["Product Name"]}
+            style={imageStyle}
+          />
+          <Stack mt="6" spacing="3">
+            <CardHeader>
+              <Heading size="md" style={titleStyle}>
+                {product["Product Name"]}
+              </Heading>
+            </CardHeader>
+            <CardBody>
+              <Text>{product["Product Price"]}</Text>
+            </CardBody>
+            <CardFooter>
+              <ButtonGroup spacing="2">
+                <Button variant="solid" colorScheme="blue">
+                  Buy now
+                </Button>
+                {/* You can add more buttons or actions here */}
+              </ButtonGroup>
+            </CardFooter>
+          </Stack>
+        </Card>
+      ))}
+    </div>
+  </>
+);
 }
+
 
 export default CardLayouts;

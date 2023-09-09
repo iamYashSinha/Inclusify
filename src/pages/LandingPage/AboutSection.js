e427c47f033b544334c7c3b2bd0b6e1b7d1b7815
dubeyanant <anantkumar312@gmail.com>
2023-09-09 11:08:09 +0530

    feat: add about section

--- src/pages/LandingPage/AboutSection.js
+++ src/pages/LandingPage/AboutSection.js
@@ -1,7 +1,29 @@
 import React from "react";
+import "../../css/AboutSection.css"; // Import your CSS file for styling
 
 const AboutSection = () => {
-  return <div></div>;
+  return (
+    <div className="container">
+      <div className="left-side">
+        <h2>TRY YOURSELF</h2>
+        <div className="search-bar">
+          <input type="text" placeholder="Search..." />
+          <div className="child">
+            <div className="left-side-or">OR</div>
+            <div className="right-side-button">
+              <button>Button</button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="right-side">
+        <h2>FEATURES</h2>
+        <p>Comprehensive Product Database</p>
+        <p>Personalized Recommendation</p>
+        <p>User-Generated Reviews</p>
+      </div>
+    </div>
+  );
 };
 
 export default AboutSection;
